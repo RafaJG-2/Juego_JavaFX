ef2302ac6694bee16d7198bc3d9dd2053b81d035
RafaJG-2 <rafa.jmgarrido@gmail.com>
2026-02-04 20:32:35 +0100

    Delete Rafa_DI_VideojuegoJavaFX directory

--- Rafa_DI_VideojuegoJavaFX/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module com.mycompany.rafa_di_videojuegojavafx {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens com.mycompany.rafa_di_videojuegojavafx to javafx.fxml;
-    exports com.mycompany.rafa_di_videojuegojavafx;
-}
